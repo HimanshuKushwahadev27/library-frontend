4c3bed280149ebf01ce3b6837d1512d96aa4889a
himanshuamaurya12 <himanshumaurya447756@gmail.com>
2026-02-14 01:27:14 +0530

    TL-42, TL-35, TL-41 init a frontend for lib

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideOAuthClient } from 'angular-oauth2-oidc';
+
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -7,6 +9,12 @@ import { provideClientHydration, withEventReplay } from '@angular/platform-brows
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideOAuthClient({
+      resourceServer:{
+        allowedUrls: ['https://localhost:8080'],
+        sendAccessToken: true
+      }
+    })
   ]
 };
